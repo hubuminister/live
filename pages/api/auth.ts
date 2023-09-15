76828ef056b125e74c0e7836c29a9c60b01153d3
hubuminister <hoob@chenzhe.dev>
2023-09-16 04:33:03 +0800

    新增授权接口

---
+++ pages/api/auth.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    console.log(req.body);
+
+  res.status(200).json({ msg: "请求成功" });
+}
